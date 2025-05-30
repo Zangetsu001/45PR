ca8a00bbe7aa225796654ac57591f1279fba3d3e
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2025-05-30 12:47:21 +0300

    Add files via upload

---
+++ Point.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp37
+{
+    class Point<T>
+    {
+        public T X { get; set; }
+        public T Y { get; set; } 
+        public Point() { }
+        public Point(T x, T y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"X = {X}\nY = {Y}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите int X: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Введите int Y: ");
+            int y = int.Parse(Console.ReadLine());
+            Point<int> point = new Point<int>(x, y);
+            point.DisplayInfo();
+
+            Console.Write("Введите uint X: ");
+            uint xUint = uint.Parse(Console.ReadLine());
+            Console.Write("Введите uint Y: ");
+            uint yUint = uint.Parse(Console.ReadLine());
+            Point<uint> pointUint = new Point<uint>(xUint, yUint);
+            pointUint.DisplayInfo();
+
+            Console.Write("Введите double X: ");
+            double xD = double.Parse(Console.ReadLine());
+            Console.Write("Введите double Y: ");
+            double yD = double.Parse(Console.ReadLine());
+            Point<double> pointD = new Point<double>(xD, yD);
+            pointD.DisplayInfo();
+
+            Console.Read();
+        }
+    }
+}
